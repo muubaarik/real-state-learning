704e65b1cd9c62e21bd1c79d9f67bc34609cc461
mubarak said <muubaarik45@gmail.com>
2024-09-06 03:24:32 -0500

    install react and tailwild the css creat  first template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="text-center bg-blue-500 text-white p-8">
+    <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+  </div>
   );
 }
 
